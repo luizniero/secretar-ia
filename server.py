8d7b4c8d804bd5b17dd77eb0823ba7d6aaa694f1
luizniero <luiz_niero@hotmail.com>
2025-06-22 23:13:10 -0300

    html + server websocket

--- server.py
+++ server.py
@@ -1,8 +1,5 @@
-# chat_server.py
 import asyncio
 import websockets
-#from agent import run_agent
-#from tools import check_availability, book_appointment
 
 async def handle_connection(websocket):
     await websocket.send("Aguarde enquanto sou conectado ao sistema de agendamento...")
@@ -10,12 +7,6 @@ async def handle_connection(websocket):
         print(f"[Usuário]: {message}")
         await websocket.send(f"Você disse: {message} — Em breve um atendente virtual irá te ajudar.")
 
-#async def handle_agentic_connection(websocket):
-#    #await websocket.send("Olá! Sou um assistente. Como posso te ajudar?")
-#    async for message in websocket:
-#        print(f"[Usuário]: {message}")
-#        response = run_agent(message)
-#        await websocket.send(response)
 
 async def main():
     async with websockets.serve(handle_connection, "localhost", 8765):
